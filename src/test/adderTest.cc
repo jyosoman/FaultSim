b3d58e2a3f110c0b761a52a1c1c65ed1378da3d3
jyosoman <jyothish.soman@gmail.com>
2016-10-24 14:58:30 +0100

    main updated

--- src/test/adderTest.cc
+++ src/test/adderTest.cc
@@ -1,14 +1,18 @@
 #include"adder.hh"
+#include<cstdlib>
 #include"testInfra.hh"
-int main(){
+int main(int argc, char*argv[]){
     KnowlesAdder<32> kadder;
     Tester<32,32,32> T(&kadder);
     cout<<"Testing begins"<<endl;
-    /* T.testVal(0x10ff00ff,0x10ff00ff); */
-    /* T.testVal(0x000001ff,0x000001ff); */
+    /* T.testVal(0xffffffff,0xffffffff); */
+    /* T.testVal(0x7000,0x9000); */
+    /* T.testVal(0x0070,0x0090); */
     /* T.testVal(0xefffffff,0xfefefefe); */
     uint32_t x,y;
-    cin>>x;
-    cin>>y;
-    T.testVal(x,y);
+    if(argc>1){
+        x=strtol(argv[1],NULL,16);
+        y=strtol(argv[2],NULL,16);
+        T.testVal(x,y);
+    }
 }
